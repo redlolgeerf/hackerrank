9891a6ea597bb64ce8670a55d9581b46e6072154
redlolgeerf <mihanne@list.ru>
2014-04-07 13:28:10 +0400

    continued work on field generator

--- generate_field.py
+++ generate_field.py
@@ -1,12 +1,30 @@
 import random
 import sys
 
+
+def random_square():
+    return int(random.random() * (field_size))
+
 field_size = int(sys.stdin.readline())
 target_ammount = int(sys.stdin.readline())
 
-targets = set([(int(random.random() * (field_size + 1)), int(random.random() * (field_size + 1 ))) for x in range(target_ammount)])
+targets = set([(random_square(), random_square())
+               for x in range(target_ammount)])
 
-r, c = ((random.random() * (field_size + 1)), (random.random() * (field_size + 1))
-field = str(field_size) + '/n' + str(r) + ' ' + str(c)
-print(targets)
+r, c = (random_square(), random_square())
 
+field = ''
+
+for y in range(field_size):
+    for x in range(field_size):
+        if x == 0:
+            field += '\n'
+        square = '-'
+        if (x, y) == (r, c):
+            square = 'b'
+        if (x, y) in targets:
+            square = 'p'
+        field += square
+
+field = str(field_size) + '\n' + str(r) + ' ' + str(c) + field
+print(field)
