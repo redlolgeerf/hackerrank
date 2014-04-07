ae9679cc36f8080c5981eec1929c54420d2a7fcd
eyeinthebrick <mihanne@list.ru>
2014-04-07 21:59:36 +0000

    started bot stohastic

--- generate_field.py
+++ generate_field.py
@@ -22,17 +22,18 @@ def make_field(field_size, target_ammount):
             if (x, y) == (r, c):
                 square = 'b'
             if (x, y) in targets:
-                square = 'p'
+                square = 'd'
             field += square
 
     field = str(field_size) + '\n' + str(r) + ' ' + str(c) + field
     return field
 
 if __name__ == '__main__':
+    print('Please enter the field size')
     field_size = int(sys.stdin.readline())
+    print('Please enter the amount of targets')
     target_ammount = int(sys.stdin.readline())
     print(make_field(field_size, target_ammount))
 else:
     field_size = int(random.random() * 100)
     target_ammount = int(random.random() * field_size / 2)
-    make_field(field_size, target_ammount)
