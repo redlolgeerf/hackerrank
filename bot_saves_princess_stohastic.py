d4a553544d74352667e72a9c29af1533c1ddb06f
eyeinthebrick <mihanne@list.ru>
2014-04-08 14:46:04 +0000

    visualisation working

--- bot_saves_princess_stohastic.py
+++ bot_saves_princess_stohastic.py
@@ -1,26 +1,51 @@
-# coding: utf-8
-
-import time
+# -*- coding: utf-8 -*-
 import curses
-import locale
-import sys
+import time
+
+import bot_saves_princess_3
 import generate_field
+import locale
+
+
+def paint_field(field, screen):
+    # вывод текста
+    for line_number, line in enumerate(field[2:]):
+        screen.addstr(line_number + 1, 1, line)
+
+    # обновление экрана
+    screen.refresh()
+    time.sleep(0.5)
+
+n = 10
+m = 5
+
+bot_x, bot_y, targets = generate_field.gen_coordinates(n, m)
+
+field = generate_field.draw_field(n, bot_x, bot_y, targets)
+field = field.split('\n')
+moves = bot_saves_princess_3.moves(field, 'd')
 
 locale.setlocale(locale.LC_ALL, '')
 stdscr = curses.initscr()
-# вывод текста
-for x in range(2):
-    field = generate_field.make_field()
-    for line_number, line in enumerate(field):
-        stdscr.addstr(line_number + 1, 1, line)
 
-    # обновление экрана
-    stdscr.refresh()
-    time.sleep(1)
+paint_field(field, stdscr)
+
+for move in moves:
+    if move == 'RIGHT':
+        bot_x += 1
+    elif move == 'LEFT':
+        bot_x -= 1
+    elif move == 'DOWN':
+        bot_y += 1
+    elif move == 'UP':
+        bot_y -= 1
+    elif move == 'CLEAN':
+        targets.remove((bot_x, bot_y))
+
+    field = generate_field.draw_field(n, bot_x, bot_y, targets)
+    field = field.split('\n')
+
+    paint_field(field, stdscr)
+
 curses.endwin()
-#for x in range(10):
-#    # sys.stdout.write('Final count down %d' % x)
-#    sys.stdout.write("%s%%   \r" % (field[:5]))
-#    #sys.stdout.write("Download progress: %d%%   \r" % (x))
-#    sys.stdout.flush()
-#    time.sleep(2)
+print('Game won!')
