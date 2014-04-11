9ddfb22f54aee444bf5eb6bc1e87058b2c268a1a
eyeinthebrick <mihanne@list.ru>
2014-04-11 17:13:18 +0400

    second task

---
+++ sorting/insertionsort1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/python3
+
+import sys
+
+
+def insert(arr, s):
+    value = arr[-1]
+    x = -2
+    while (arr[x] > value) and (x * -1 <= s):
+        arr[x + 1] = arr[x]
+        print(" ".join([str(i) for i in arr]))
+        x -= 1
+    arr[x+1] = value
+    print(" ".join([str(i) for i in arr]))
+
+if __name__ == '__main__':
+#     size = sys.stdin.readline().strip()
+#     array = sys.stdin.readline().strip().split()
+#     array = [int(x) for x in array]
+    array = [2, 4, 6, 8, 1]
+    insert(array, 5)
