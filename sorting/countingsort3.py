e372c9036f21145a7c1f55e7edecfbe5ff5b8f1f
eyeinthebrick <mihanne@list.ru>
2014-04-17 23:48:31 +0400

    third task for counting sort

--- sorting/countingsort3.py
+++ sorting/countingsort3.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 # !/usr/bin/python3
+# -*- coding: utf-8 -*-
 
 import sys
 
@@ -10,16 +10,18 @@ def counting(arr, s):
         appearences[x] = 0
     for value in arr:
         appearences[value] += 1
-    result = []
-    for x in range(100):
-        if appearences[x] != 0:
-            for a in range(appearences[x]):
-                result.append(x)
-    return result
+    return appearences
 
 if __name__ == '__main__':
-#     size = int(sys.stdin.readline().strip())
-#     array = sys.stdin.readline().strip().split()
-    array = '63 25 73 1 98 73 56 84 86 57 16 83 8 25 81 56 9 53 98 67 99 12 83 89 80 91 39 86 76 85 74 39 25 90 59 10 94 32 44 3 89 30 27 79 46 96 27 32 18 21 92 69 81 40 40 34 68 78 24 87 42 69 23 41 78 22 6 90 99 89 50 30 20 1 43 3 70 95 33 46 44 9 69 48 33 60 65 16 82 67 61 32 21 79 75 75 13 87 70 33'.strip().split()
+    size = int(sys.stdin.readline().strip())
+    array = []
+    for x in range(size):
+        array.append(sys.stdin.readline().strip().split()[0])
     array = [int(i) for i in array]
-    print(" ".join([str(i) for i in counting(array, len(array))]))
+    arr = counting(array, size)
+    counter = 0
+    result = []
+    for x in range(100):
+        counter += arr[x]
+        result.append(counter)
+    print(" ".join(str(i) for i in result))
